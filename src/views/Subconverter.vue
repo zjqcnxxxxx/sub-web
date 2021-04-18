d0e4cbcdf37b11a809feadac2dcfafefaa4a103f
zhonfly <icesshadows@gmail.com>
2021-04-18 14:59:24 +0800

    fix: customBackend value not use .env

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -553,7 +553,7 @@ export default {
   },
   mounted() {
     this.form.clientType = "clash";
-    this.form.customBackend = "https://sub-beta.now.sh/sub?";
+    this.form.customBackend = defaultBackend;
     this.form.remoteConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online.ini";
     this.getBackendVersion();
   },
