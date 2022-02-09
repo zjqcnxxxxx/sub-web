544891ad0f656400d7d6e9fc0ec90ce718d22eb7
peggypapa <54925028+zjqcnxxxxx@users.noreply.github.com>
2022-02-10 00:02:33 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -256,7 +256,7 @@ export default {
         },
         customBackend: {
           "localhost:25500 本地版": "http://localhost:25500/sub?",
-          "subb.zjqrex.tk": "https://subb.zjqrex.tk/sub?",
+          "subb.304105407.xyz": "https://subb.304105407.xyz/sub?",
           "subcon.dlj.tf(subconverter作者提供-稳定)":
             "https://subcon.dlj.tf/sub?",
           "api.dler.io(sub作者&lhie1提供-稳定)": "https://api.dler.io/sub?",
@@ -265,7 +265,7 @@ export default {
         },
         backendOptions: [
           { value: "http://localhost:25500/sub?" },
-          { value: "https://subb.zjqrex.tk/sub?" },
+          { value: "https://subb.304105407.xyz/sub?" },
           { value: "https://subcon.dlj.tf/sub?" },
           { value: "https://api.dler.io/sub?" },
           { value: "https://api.wcc.best/sub?" },
